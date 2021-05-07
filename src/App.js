42766a814b66e56804cb0f85909addffc79e2ecc
Sane404 <Sane9@i.ua>
2021-05-07 10:16:25 +0300

    check

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import About from "./About";
 import Navbar from "./Navbar";
 import Header from "./Header";
 import SingleImage from "./SingleImage";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     <>
